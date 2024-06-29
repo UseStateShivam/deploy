9269cae32785277434b4b282ce655aa609946869
Shivam Verma <mcrafter147@gamil.com>
2024-06-29 16:17:12 +0530

    f

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+module.exports = {
+    exportPathMap: async function (
+      defaultPathMap,
+      { dev, dir, outDir, distDir, buildId }
+    ) {
+      return {
+        '/': { page: '/' },
+        '/register': { page: '/register' }
+      }
+    }
+  }
+  
